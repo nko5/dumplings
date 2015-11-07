a515a1268c84945165f171fd1ba3ea11fbada7b5
Piotr Kowalski <piecioshka@gmail.com>
2015-11-07 11:00:38 +0100

    Add map in Gameboy style

--- public/scripts/states/MenuState.es6.js
+++ public/scripts/states/MenuState.es6.js
@@ -1,10 +1,19 @@
 export default class MenuState extends Phaser.State {
     preload() {
-        this.load.image('geek', '');
+        // this.load.image('geek', '');
+        this.game.load.spritesheet('gameboy-tileset', 'assets/tilesets/gameboy-tileset.png', 16, 16);
+        this.load.tilemap('map-1',  'assets/maps/map-1.json', null, Phaser.Tilemap.TILED_JSON);
     }
 
     create() {
         console.log(this.game.player);
+
+        let map = this.game.add.tilemap('map-1');
+        map.addTilesetImage('gameboy-tileset');
+        map.setCollisionBetween(1, 256);
+
+        let worldLayer = map.createLayer('Tile Layer 1');
+        worldLayer.resizeWorld();
     }
 
     update() {
