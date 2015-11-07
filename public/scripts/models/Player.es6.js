bfc0c4c72336f3d22e2987f157181e653cb73691
Piotr Kowalski <piecioshka@gmail.com>
2015-11-07 14:46:35 +0100

    Support player control (physics)

--- public/scripts/models/Player.es6.js
+++ public/scripts/models/Player.es6.js
@@ -1,22 +1,33 @@
 export default class Player {
     game = null;
     name = 'no name';
+    sprite = null;
 
     constructor(game) {
         this.game = game;
-        console.log('create new player');
     }
 
     render() {
-        let sprite = this.game.add.sprite(50, 90, 'mms');
+        let sprite = this.sprite = this.game.add.sprite(50, 70, 'mms');
 
         sprite.anchor.setTo(0, 1);
         // sprite.scale.setTo(0.8, 0.8);
-        // sprite.scale.x = -1;
 
         sprite.animations.add('stay', [2, 4, 8, 11, 14].map((i) => i - 1), 3, true, true);
         sprite.animations.add('walk', [1, 5, 7, 32, 13, 15].map((i) => i - 1), 8, true, true);
 
-        sprite.animations.play('walk');
+        sprite.animations.play('stay');
+
+        this._enablePhysics();
+    }
+
+    _enablePhysics() {
+        this.game.physics.arcade.enable(this.sprite);
+
+        this.sprite.body.bounce.y = 0.2;
+        this.sprite.body.collideWorldBounds = true;
+        this.sprite.body.linearDamping = 1;
+
+        // this.sprite.body.setSize(30, 30, 0, 10);
     }
 }
--- public/scripts/states/GameState.es6.js
+++ public/scripts/states/GameState.es6.js
@@ -2,6 +2,8 @@ import AbstractState from './AbstractState';
 
 export default class GameState extends AbstractState {
     preload() {
+        this.load.json('positions-1', 'assets/items/positions-1.json');
+
         this.load.spritesheet('gameboy-tileset', 'assets/tilesets/gameboy-tileset.png', 16, 16);
         this.load.tilemap('map-1', 'assets/maps/map-1.json', null, Phaser.Tilemap.TILED_JSON);
 
@@ -13,23 +15,84 @@ export default class GameState extends AbstractState {
     }
 
     create() {
+        this._setupWorld();
         this.game.player.render();
+        this._setupItems();
+    }
+
+    _setupWorld() {
+        this.physics.startSystem(Phaser.Physics.ARCADE);
+        this.physics.arcade.gravity.y = 250;
 
         let map = this.game.add.tilemap('map-1');
         map.addTilesetImage('gameboy-tileset');
-        map.setCollisionBetween(1, 256);
+        map.setCollision([
+            5, 14, 16, 25,  // world bounds
+            45, 46, 47,     // ground 1
+            65, 66, 67,     // ground 2
+            40, 41, 42,     // ground 3
+            0, 38, 48,      // plates
+            35, 36, 37,     // ground 4
+            8, 18, 28       // ground 5
+        ].map((i) => i + 1));
 
-        let worldLayer = map.createLayer('Tile Layer 1');
-        worldLayer.resizeWorld();
+        this.worldLayer = map.createLayer('Tile Layer 1');
+        this.worldLayer.resizeWorld();
+    }
 
-        this.game.physics.startSystem(Phaser.Physics.ARCADE);
+    _setupItems() {
+        let items = this.game.items = this.add.group();
+
+        let places = this.cache.getJSON('positions-1');
+
+        places.forEach(({ x, y }) => {
+            let item = this.add.tileSprite(x * 16, y * 16, 16, 16, 'gameboy-tileset', 2);
+            this.physics.arcade.enable(item);
+            item.body.allowGravity = false;
+            items.add(item);
+        });
     }
 
     update() {
+        this._handleCollision();
+        this._handleKeyboard();
+    }
 
+    _handleCollision() {
+        this.game.physics.arcade.collide(this.game.player.sprite, this.worldLayer);
+        this.game.physics.arcade.collide(this.game.player.sprite, this.game.items, (player, item) => {
+            item.destroy();
+        });
+    }
+
+    _handleKeyboard() {
+        let player = this.game.player;
+        let sprite = player.sprite;
+        let body = sprite.body;
+        let keyboard = this.input.keyboard;
+
+        body.velocity.x = 0;
+
+        if (keyboard.isDown(Phaser.Keyboard.LEFT)) {
+            body.velocity.x = -150;
+            // sprite.scale.x = -1;
+            sprite.animations.play('walk');
+        } else if (keyboard.isDown(Phaser.Keyboard.RIGHT)) {
+            body.velocity.x = 150;
+            // sprite.scale.x = 1;
+            sprite.animations.play('walk');
+        } else {
+            sprite.animations.play('stay');
+        }
+
+        if (body.onFloor() && (keyboard.isDown(Phaser.Keyboard.UP) || keyboard.isDown(Phaser.Keyboard.SPACEBAR))) {
+            body.velocity.y = -200;
+        }
     }
 
     render() {
-
+        // let player = this.game.player;
+        // this.game.debug.bodyInfo(player.sprite, 25, 25);
+        // this.game.debug.body(player.sprite);
     }
 }
