8798fd90212d76521ff903bd6d05722a918b0133
Piotr Kowalski <piecioshka@gmail.com>
2015-11-07 17:15:40 +0100

    Remove depracated app.configure

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var app = express();
+var port = 8080;
+
+var env = app.get('env');
+console.log(env);
 
 // Create a static file server
-app.configure(function () {
-    app.use(express.static(__dirname + '/public'));
-});
+app.use(express.static(__dirname + '/public'));
 
-var port = 8080;
 app.listen(port);
+
 console.log('Express server started on port %s', port);
