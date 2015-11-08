e1a1d57102a76066325b92e71589e001026c69ad
Piotr Kowalski <piecioshka@gmail.com>
2015-11-08 11:38:08 +0100

    Use env.js

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = 8080;
 
+// ENV
+require('./server/env')(app);
+
 // HTTP
 require('./server/http-server')(app, express, http, port);
 
--- server/env.js
+++ server/env.js
@@ -1,11 +1,13 @@
-var env = app.get('env');
+module.exports = function (app) {
+    var env = app.get('env');
 
-switch (env) {
-    case 'development':
-        console.log('DEVELOPMENT');
-        break;
+    switch (env) {
+        case 'development':
+            console.log('DEVELOPMENT');
+            break;
 
-    case 'production':
-        console.log('PRODUCTION');
-        break;
-}
+        case 'production':
+            console.log('PRODUCTION');
+            break;
+    }
+};
